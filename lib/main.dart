92776b7e5a894b48aa6f3ced187c655dc0401d09
Nithish Kumar <nithish25may2005@gmail.com>
2024-12-15 14:42:34 +0800

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:shoppingapp/Admin/admin_login.dart';
+import 'package:shoppingapp/pages/bottomnav.dart';
 import 'package:shoppingapp/pages/home.dart';
-import 'package:shoppingapp/pages/onboarding.dart';
+import 'package:shoppingapp/pages/login.dart';
+import 'package:shoppingapp/pages/signup.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase with platform-specific options
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,7 +30,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Home(),
+      home: AdminLogin(),
     );
   }
 }
